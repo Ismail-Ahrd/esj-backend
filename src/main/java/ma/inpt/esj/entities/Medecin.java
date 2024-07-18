efbc775cd66943afc9216ca0c4b28e5ef9f5c1bd
Chahid Hadifi <chahidhadifi@gmail.com>
2024-07-18 18:40:31 +0100

    feat: add columns to `Medecin` entity

--- src/main/java/ma/inpt/esj/entities/Medecin.java
+++ src/main/java/ma/inpt/esj/entities/Medecin.java
@@ -3,21 +3,32 @@ package ma.inpt.esj.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.List;
+
 @Entity
 @Data
-@AllArgsConstructor @NoArgsConstructor @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Medecin {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String cin;
+
     @Column(unique = true)
     private String inpe;
+
     @Column(unique = true)
     private String ppr;
+
     private boolean estMedcinESJ;
+
     private boolean estGeneraliste;
+
     private String specialite;
+        
     @OneToOne(cascade = CascadeType.ALL)
     private InfoUser infoUser;
 
@@ -26,6 +37,30 @@ public class Medecin {
     private boolean confirmed =false;
 
     private boolean isFirstAuth=true;
+
+    @Column(columnDefinition = "TEXT")
+    private String aProposDeMoi;
+
+    @ElementCollection
+    @CollectionTable(name = "langues_parlees", joinColumns = @JoinColumn(name = "medecin_id"))
+    @Column(name = "langue")
+    private List<String> languesParlees;
+    
+    @ElementCollection
+    @CollectionTable(name = "specialites_medicales", joinColumns = @JoinColumn(name = "medecin_id"))
+    @Column(name = "specialite")
+    private List<String> specialites;
+
+    @ElementCollection
+    @CollectionTable(name = "education", joinColumns = @JoinColumn(name = "medecin_id"))
+    private List<Education> educations;
+
+    @ElementCollection
+    @CollectionTable(name = "experience", joinColumns = @JoinColumn(name = "medecin_id"))
+    private List<Experience> experiences;
+
+    @Column(name = "evaluation", columnDefinition = "INTEGER DEFAULT 0")
+    private Integer evaluation;
 
     @OneToMany(mappedBy = "medecin", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Jeune> patients;
