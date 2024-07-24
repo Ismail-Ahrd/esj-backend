f1cdf2b74073d1c3a5f507eb60e31ea33313944c
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-07-24 21:38:52 +0100

    add validator

--- src/main/java/ma/inpt/esj/repositories/InfoUserRepository.java
+++ src/main/java/ma/inpt/esj/repositories/InfoUserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface InfoUserRepository extends JpaRepository<InfoUser,Long> {
     Optional<InfoUser> findByMail(String mail);
     boolean existsByMail(String mail);
+
+    boolean existsByNumTel(String numTel);
 }
