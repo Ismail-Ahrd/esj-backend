9af2fb84f80f4b5241c1cb2bd4714079dfc9b335
unknown <boudiwanbouchra@gmail.com>
2024-08-31 02:16:07 +0200

    Your commit message

--- src/main/java/ma/inpt/esj/controllers/ConfirmationMailController.java
+++ src/main/java/ma/inpt/esj/controllers/ConfirmationMailController.java
@@ -2,11 +2,13 @@ package ma.inpt.esj.controllers;
 
 
 import lombok.AllArgsConstructor;
+import ma.inpt.esj.entities.Administrateur;
 import ma.inpt.esj.entities.Jeune;
 import ma.inpt.esj.entities.Medecin;
 import ma.inpt.esj.entities.ProfessionnelSante;
 import ma.inpt.esj.exception.*;
 import ma.inpt.esj.services.ConfirmeMailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +17,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 @AllArgsConstructor
 public class ConfirmationMailController {
-
+    @Autowired
     private ConfirmeMailService confirmeMailService;
 
     @GetMapping("/register/confirmation")
@@ -29,13 +31,15 @@ public class ConfirmationMailController {
 
         } else if (person instanceof Jeune) {
             return new RedirectView("http://localhost:3000/auth/jeunes");
+        } else if (person instanceof Administrateur) {
+            return new RedirectView("http://localhost:3000/auth/administrateur");
         } else {
             throw new ConfirmationMailException("Unknown person type");
         }
     }
 
     @PostMapping("/register/resend-token")
-    public ResponseEntity<String> resendToken(@RequestParam("email") String email) throws JeuneNotFoundException, UserNotFoundException, MedecinNotFoundException {
+    public ResponseEntity<String> resendToken(@RequestParam("email") String email) throws JeuneNotFoundException, UserNotFoundException, MedecinNotFoundException, ProfessionnelNotFoundException,AdministrateurNotFoundException {
         System.out.println("***************************************************");
         confirmeMailService.resendToken(email);
         return ResponseEntity.ok("Token resent successfully");
